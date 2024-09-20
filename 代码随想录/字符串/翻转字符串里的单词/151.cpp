f623cff118a67c2f90858e94322a19ac02c9955f
catherinexrk <helloworldxrk@gmail.com>
2024-09-20 10:32:43 +0800

    更新一下

--- 代码随想录/字符串/翻转字符串里的单词/151.cpp
+++ 代码随想录/字符串/翻转字符串里的单词/151.cpp
@@ -31,7 +31,30 @@ public:
 	}
 };
 
-class Solution{
+class Solution2{
+public:
+	string reverseWords(string s) {
+		vector<string> result;
+		stringstream temp(s);//根据空格进行分割字符
+		string word;//接收temp中的分割
+		for(temp >> word){
+			result.push_back(word);
+		}
+		
+		reverse(result.begin(),result.end());//反转数组
+		string return1;
+		for(int i = 0;i < result.size();i++){
+			return1 += result[i];
+			if(i != result.size() - 1){
+				return1 += ' ';
+			}
+		}
+		return return1;
+		
+	}
+};
+
+class Solution {
 public:
 	string reverseWords(string s) {
 		
